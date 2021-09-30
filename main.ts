8a8bb510496cccbd14edc93d4730d611fd771acc
kim-cho-i <90668331+kim-cho-i@users.noreply.github.com>
2021-09-30 10:54:54 +0900

    내가짠거 선생님이짠거

--- main.ts
+++ main.ts
@@ -7,8 +7,19 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         music.playTone(392, music.beat(BeatFraction.Whole))
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        music.stopAllSounds()
     }
+    basic.showIcon(IconNames.Sad)
 })
+input.onButtonPressed(Button.A, function () {
+    경비전원 = 1
+})
+input.onButtonPressed(Button.B, function () {
+    경비전원 = 0
+})
+let 경비전원 = 0
 radio.setGroup(5)
 basic.forever(function () {
 	
